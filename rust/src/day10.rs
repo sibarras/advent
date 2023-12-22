face1e53480939aca9167451b247494c0eebf2fd
Samuel Ibarra <ing.samuelibarra@gmail.com>
2023-12-22 15:39:01 -0500

    Update on naming and adding latest days
    This is to maintain order on the IDE's

---
+++ rust/src/day10.rs
@@ -0,0 +1,228 @@
+use std::borrow::BorrowMut;
+use std::ops::Not;
+
+use crate::advent_utils::{AdventSolution, GenericResult};
+
+use crate::advent_test;
+
+pub struct Solution;
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+struct Pipe {
+    char: char,
+    position: (usize, usize),
+    start: (usize, usize),
+    end: (usize, usize),
+    in_maze: bool,
+}
+
+impl From<((usize, usize), char)> for Pipe {
+    fn from(((x, y), c): ((usize, usize), char)) -> Self {
+        let (st, end) = match c {
+            '|' => ((0, 1), (0, -1)),
+            '-' => ((1, 0), (-1, 0)),
+            'L' => ((0, 1), (1, 0)),
+            'J' => ((0, 1), (-1, 0)),
+            '7' => ((0, -1), (-1, 0)),
+            'F' => ((0, -1), (1, 0)),
+            '.' => ((0, 0), (0, 0)),
+            'S' => ((0, 0), (0, 0)),
+            _ => panic!("Invalid pipe character: {}", c),
+        };
+
+        let start_pos = ((x as isize + st.0) as usize, (y as isize - st.1) as usize);
+        let end_pos = ((x as isize + end.0) as usize, (y as isize - end.1) as usize);
+
+        Self {
+            char: c,
+            start: start_pos,
+            end: end_pos,
+            position: (x, y),
+            in_maze: false,
+        }
+    }
+}
+
+impl Pipe {
+    /// Returns the next pipe and the position of the prev pipe.
+    fn next(&self, from: (usize, usize), map: &mut [Vec<Pipe>]) -> ((usize, usize), Pipe) {
+        let (x, y) = self.position;
+        map[y][x].in_maze = true;
+
+        let (pos, pipe) = if from == self.position {
+            let (ymin, ymax) = (y.saturating_sub(1), (y + 1).min(map.len() - 1));
+            let (xmin, xmax) = (x.saturating_sub(1), (x + 1).min(map[0].len() - 1));
+            let pipe = map[ymin..=ymax]
+                .iter_mut()
+                .flat_map(|row| row[xmin..=xmax].iter_mut())
+                .find(|pipe| {
+                    "S.".contains(pipe.char).not()
+                        && (pipe.start == self.position || pipe.end == self.position)
+                })
+                .expect("No pipe connected to S.");
+            ((x, y), pipe)
+        } else if from == self.start {
+            (self.position, map[self.end.1][self.end.0].borrow_mut())
+        } else if from == self.end {
+            (self.position, map[self.start.1][self.start.0].borrow_mut())
+        } else {
+            panic!("Invalid pipe position: {:?}", from);
+        };
+
+        pipe.in_maze = true;
+        (pos, *pipe)
+    }
+
+    fn cast_s_to_piece(&self, init: &Pipe, end: &Pipe) -> Pipe {
+        let mut pipe = "7FJL|-"
+            .chars()
+            .map(|c| Pipe::from((self.position, c)))
+            .find(|pipe| {
+                pipe.start == init.position && pipe.end == end.position
+                    || pipe.start == end.position && pipe.end == init.position
+            })
+            .expect("Is not possible to cast S to a piece");
+        pipe.in_maze = true;
+        pipe
+    }
+}
+
+fn get_inners_count(map: &[Vec<Pipe>]) -> usize {
+    map.iter()
+        .map(|line| {
+            let mut within = 0;
+            let mut count = 0;
+            let mut expecting = None;
+            for pipe in line {
+                if pipe.in_maze && "FL".contains(pipe.char) {
+                    expecting = match pipe.char {
+                        'F' => Some('J'),
+                        'L' => Some('7'),
+                        _ => None,
+                    };
+                } else if pipe.in_maze && pipe.char != '-' {
+                    count += match expecting {
+                        Some(c) if pipe.char == c => 1,
+                        Some(_) => 0,
+                        None => 1,
+                    };
+                    expecting = None;
+                } else if count % 2 == 1 && !pipe.in_maze {
+                    within += 1;
+                }
+            }
+            within
+        })
+        .sum()
+}
+
+impl AdventSolution for Solution {
+    fn part1(input: Vec<String>) -> GenericResult<String> {
+        let mut map = input
+            .iter()
+            .enumerate()
+            .map(|(y, line)| {
+                line.char_indices()
+                    .map(|(x, char)| Pipe::from(((x, y), char)))
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>();
+
+        let mut pipe = *map
+            .iter()
+            .find_map(|row| row.iter().find(|pipe| pipe.char == 'S'))
+            .expect("No S pipe found :(");
+
+        let mut pos = pipe.position;
+
+        let mut count: usize = 0;
+        // let mut positions = Vec::new();
+        loop {
+            (pos, pipe) = pipe.next(pos, &mut map);
+            count += 1;
+            // positions.push(pos);
+            if pipe.char == 'S' {
+                break;
+            }
+        }
+
+        Ok((count / 2).to_string())
+    }
+
+    fn part2(input: Vec<String>) -> GenericResult<String> {
+        let mut map = input
+            .iter()
+            .enumerate()
+            .map(|(y, line)| {
+                line.char_indices()
+                    .map(|(x, char)| Pipe::from(((x, y), char)))
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>();
+
+        let mut pipe = *map
+            .iter()
+            .find_map(|row| row.iter().find(|pipe| pipe.char == 'S'))
+            .expect("No S pipe found :(");
+
+        let mut pos = pipe.position;
+
+        // let mut count: usize = 0;
+        // let mut maze = Vec::with_capacity(map.len() * map[0].len());
+
+        // using the first step as the begining.
+        (pos, pipe) = pipe.next(pos, &mut map);
+        let end = pipe;
+        let mut init = pipe;
+
+        while pipe.char != 'S' {
+            init = pipe;
+            (pos, pipe) = pipe.next(pos, &mut map);
+            // count += 1;
+            // maze.push(Pipe { ..*pipe });
+        }
+
+        for row in map.iter_mut() {
+            if let Some(v) = row.iter_mut().find(|pipe| pipe.char == 'S') {
+                *v = pipe.cast_s_to_piece(&init, &end);
+                break;
+            }
+        }
+
+        // let maze_pos = maze.into_iter().map(|p| p.position).collect::<Vec<_>>();
+        let inners = get_inners_count(&map);
+
+        Ok(inners.to_string())
+    }
+}
+
+advent_test!(
+    "../inputs/tests/day10.txt",
+    "8",
+    "../inputs/tests/day10_2.txt",
+    "4"
+);
+
+#[cfg(test)]
+mod test2 {
+    use super::*;
+    use crate::advent_utils::read_input;
+
+    // advent_test!(
+    //     "../inputs/tests/day10_3.txt",
+    //     "8",
+    //     "../inputs/tests/day10_4.txt",
+    //     "10"
+    // );
+    #[test]
+    fn test_solution_3() {
+        let input = read_input("../inputs/tests/day10_3.txt").unwrap();
+        assert_eq!(Solution::part2(input).unwrap(), "8");
+    }
+
+    #[test]
+    fn test_solution_4() {
+        let input = read_input("../inputs/tests/day10_4.txt").unwrap();
+        assert_eq!(Solution::part2(input).unwrap(), "10");
+    }
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,25 +1,27 @@
 mod advent_utils;
-mod day1;
-mod day2;
-mod day3;
-mod day4;
-mod day5;
-mod day6;
-mod day7;
-mod day8;
-mod day9;
+mod day01;
+mod day02;
+mod day03;
+mod day04;
+mod day05;
+mod day06;
+mod day07;
+mod day08;
+mod day09;
+mod day10;
 
 use advent_utils::run;
 
 fn main() -> advent_utils::GenericResult<()> {
-    run::<day1::Solution>("../inputs/day1_1.txt", "../inputs/day1_2.txt")?;
-    run::<day2::Solution>("../inputs/day2_1.txt", "../inputs/day2_1.txt")?;
-    run::<day3::Solution>("../inputs/day3_1.txt", "../inputs/day3_1.txt")?;
-    run::<day4::Solution>("../inputs/day4_1.txt", "../inputs/day4_1.txt")?;
-    run::<day5::Solution>("../inputs/day5_1.txt", "../inputs/day5_1.txt")?;
-    run::<day6::Solution>("../inputs/day6.txt", "../inputs/day6.txt")?;
-    run::<day7::Solution>("../inputs/day7.txt", "../inputs/day7.txt")?;
-    run::<day8::Solution>("../inputs/day8.txt", "../inputs/day8.txt")?;
-    run::<day9::Solution>("../inputs/day9.txt", "../inputs/day9.txt")?;
+    run::<day01::Solution>("../inputs/day1_1.txt", "../inputs/day1_2.txt")?;
+    run::<day02::Solution>("../inputs/day2_1.txt", "../inputs/day2_1.txt")?;
+    run::<day03::Solution>("../inputs/day3_1.txt", "../inputs/day3_1.txt")?;
+    run::<day04::Solution>("../inputs/day4_1.txt", "../inputs/day4_1.txt")?;
+    run::<day05::Solution>("../inputs/day5_1.txt", "../inputs/day5_1.txt")?;
+    run::<day06::Solution>("../inputs/day6.txt", "../inputs/day6.txt")?;
+    run::<day07::Solution>("../inputs/day7.txt", "../inputs/day7.txt")?;
+    run::<day08::Solution>("../inputs/day8.txt", "../inputs/day8.txt")?;
+    run::<day09::Solution>("../inputs/day9.txt", "../inputs/day9.txt")?;
+    run::<day10::Solution>("../inputs/day10.txt", "../inputs/day10.txt")?;
     Ok(())
 }
