70d9de15089641938b97af4c970e655660fd5548
Samuel Ibarra <ing.samuelibarra@gmail.com>
2024-02-14 09:07:10 -0500

    Rewrite code

--- rust/src/days/day16.rs
+++ rust/src/days/day16.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use crate::{
     advent_tests,
     advent_utils::{AdventSolution, GenericResult},
@@ -7,149 +9,52 @@ const MIRRORS: [u8; 4] = [b'/', b'\\', b'|', b'-'];
 
 pub struct Solution;
 
-#[derive(Debug, Clone, PartialEq)]
-struct Position(usize, usize);
-
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 enum Direction {
-    Left,
-    Right,
-    Up,
-    Down,
+    Left { x: usize, y: usize },
+    Right { x: usize, y: usize },
+    Up { x: usize, y: usize },
+    Down { x: usize, y: usize },
 }
 
-struct Movement {
-    direction: Direction,
-    position: Position,
+impl Direction {
+    fn next(&self, ctx: &[&[u8]]) -> (Option<Self>, Option<Self>) {}
+
+    fn ray_distance(&self, ctx: &[&[u8]]) -> usize {}
 }
 
-fn check_next(
-    Movement {
-        direction,
-        position,
-    }: Movement,
-    input: &[&[u8]],
-    mut used: Vec<Position>,
-) -> usize {
-    if used.contains(&position) {
-        return 0;
-    }
-
-    let Position(x, y) = position;
-    let new_position = match direction {
-        Direction::Right => input[y]
-            .iter()
-            .enumerate()
-            .skip(x)
-            .find(|(_, c)| MIRRORS.contains(c) && **c != b'-')
-            .map(|(idx, _)| (idx, y)),
-        Direction::Left => input[y][..=x]
-            .iter()
-            .enumerate()
-            .rev()
-            .find(|(_, c)| MIRRORS.contains(c) && **c != b'-')
-            .map(|(idx, _)| (idx, y)),
-        Direction::Down => (y..input.len())
-            .map(|idx| (idx, input[idx][x]))
-            .find(|(_, c)| MIRRORS.contains(c) && *c != b'|')
-            .map(|(idx, _)| (x, idx)),
-        Direction::Up => (0..=y)
-            .map(|idx| (idx, input[idx][x]))
-            .rev()
-            .find(|(_, c)| MIRRORS.contains(c) && *c != b'|')
-            .map(|(idx, _)| (x, idx)),
-    };
-
-    if let Some((xf, yf)) = new_position {
-        used.push(Position(xf, yf));
-        match (input[yf][xf], direction) {
-            (b'|', Direction::Left) | (b'|', Direction::Right) => {
-                check_next(
-                    Movement {
-                        direction: Direction::Up,
-                        position: Position(xf, yf),
-                    },
-                    input,
-                    used.clone(),
-                ) + check_next(
-                    Movement {
-                        direction: Direction::Down,
-                        position: Position(xf, yf),
-                    },
-                    input,
-                    used,
-                )
+fn calculate(ctx: &[&[u8]]) -> usize {
+    let mut queue = Vec::with_capacity(
+        ctx.iter()
+            .map(|ln| ln.iter().filter(|a| MIRRORS.contains(a)).count())
+            .sum::<usize>(),
+    );
+    queue.push(Direction::Right { x: 0, y: 0 });
+    let mut used: HashSet<Direction> = Default::default();
+    while let Some(new_direction) = queue.pop() {
+        used.insert(new_direction.clone());
+        if let (Some(new_direction), other) = new_direction.next(ctx) {
+            if !used.contains(&new_direction) {
+                queue.push(new_direction);
             }
-            (b'|', Direction::Up) | (b'-', Direction::Down) => {
-                check_next(
-                    Movement {
-                        direction: Direction::Left,
-                        position: Position(xf, yf),
-                    },
-                    input,
-                    used.clone(),
-                ) + check_next(
-                    Movement {
-                        direction: Direction::Right,
-                        position: Position(xf, yf),
-                    },
-                    input,
-                    used,
-                )
+            if let Some(other_direction) = other {
+                if !used.contains(&other_direction) {
+                    queue.push(other_direction);
+                }
             }
-            (b'/', Direction::Right) | (b'\\', Direction::Left) => check_next(
-                Movement {
-                    direction: Direction::Up,
-                    position: Position(xf, yf),
-                },
-                input,
-                used,
-            ),
-            (b'\\', Direction::Right) | (b'/', Direction::Left) => check_next(
-                Movement {
-                    direction: Direction::Down,
-                    position: Position(xf, yf),
-                },
-                input,
-                used,
-            ),
-            (b'/', Direction::Up) | (b'\\', Direction::Down) => check_next(
-                Movement {
-                    direction: Direction::Right,
-                    position: Position(xf, yf),
-                },
-                input,
-                used,
-            ),
-            (b'\\', Direction::Up) | (b'/', Direction::Down) => check_next(
-                Movement {
-                    direction: Direction::Left,
-                    position: Position(xf, yf),
-                },
-                input,
-                used,
-            ),
-            _ => unreachable!("should never happen"),
-        }
-    } else {
-        match direction {
-            Direction::Right => input[y].len() - (x + 1),
-            Direction::Left => x + 1,
-            Direction::Down => input.len() - (y + 1),
-            Direction::Up => y + 1,
         }
     }
+    used.len()
+        + used
+            .into_iter()
+            .map(|direction| direction.ray_distance(ctx))
+            .sum::<usize>();
 }
 
 impl AdventSolution for Solution {
     fn part1(input: Vec<String>) -> GenericResult<usize> {
-        let movement = Movement {
-            direction: Direction::Right,
-            position: Position(0, 0),
-        };
         let new_input = input.iter().map(|s| s.as_bytes()).collect::<Vec<_>>();
-        let result = check_next(movement, &new_input, vec![Position(0, 0)]);
-        Ok(result)
+        Ok(calculate(&new_input))
     }
 
     fn part2(input: Vec<String>) -> GenericResult<usize> {
