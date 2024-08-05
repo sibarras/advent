b604c028d5993b11de6d0f305f2cdf35c51c2d2c
Samuel Ibarra <ing.samuelibarra@gmail.com>
2024-08-04 19:30:22 -0500

    Trying to add ocaml/ still days are not imported

--- ocaml/bin/advent_utils.ml
+++ ocaml/bin/advent_utils.ml
@@ -1,7 +1,22 @@
-let run = print_endline
-
 let read_file path =
-  let f = path |> open_in in
-    let list = []
-    to_list f:wq:wq
-    
+  let chan = open_in path in
+  let rec append_lines acc =
+    try
+      let line = input_line chan in
+        append_lines (line :: acc)
+    with End_of_file ->
+      close_in chan;
+      List.rev acc
+    in
+    append_lines []
+
+type adventSolution = {
+  part_1: string list -> int;
+  part_2: string list -> int;
+}
+
+let run path solution =
+  let lines = read_file path in
+    lines |> solution.part_1 |> string_of_int |> String.cat "\tPart 1 => " |> print_string;
+    lines |> solution.part_2 |> string_of_int |> String.cat "\tPart 2 => " |> print_string
+
