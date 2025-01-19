d0f4ea340c22de99559a3b3f058b208dda79b2bd
Samuel Ibarra <ing.samuelibarra@gmail.com>
2025-01-19 05:48:37 -0500

    fix p1

--- zig/src/2024/days/day01.zig
+++ zig/src/2024/days/day01.zig
@@ -5,35 +5,42 @@ pub const Solution = .{
 };
 
 fn part1(data: []u8) anyerror!i32 {
-    const lines = std.mem.splitScalar(u8, data, '\n');
-    var l1 = std.ArrayList(usize){};
-    var l2 = std.ArrayList(usize){};
+    var lines = std.mem.splitScalar(u8, data, '\n');
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const alloc = gpa.allocator();
+    var l1 = std.ArrayList(usize).init(alloc);
+    defer l1.deinit();
+    var l2 = std.ArrayList(usize).init(alloc);
+    defer l2.deinit();
 
     while (lines.next()) |line| {
-        const sep = std.mem.indexOf(u8, line, ' ').?;
+        const sep = std.mem.indexOfScalar(u8, line, ' ') orelse continue;
         var idx: usize = 1;
-        while (line[sep + 1] == ' ') {
+        while (line[sep + idx] == ' ') {
             idx += 1;
         }
-        const next = idx;
+        const next = sep + idx;
         const f = try std.fmt.parseInt(usize, line[0..sep], 10);
         const l = try std.fmt.parseInt(usize, line[next..], 10);
-        l1.append(f);
-        l2.append(l);
+        try l1.append(f);
+        try l2.append(l);
     }
 
-    std.mem.sort(l1, {}, std.sort.asc(u8));
-    std.mem.sort(l2, {}, std.sort.asc(u8));
+    std.mem.sort(usize, l1.items, {}, std.sort.asc(usize));
+    std.mem.sort(usize, l2.items, {}, std.sort.asc(usize));
 
-    var t: i32 = 0;
-    for (0..l1.size()) |i| {
-        const v2: i32 = @intCast(l2[i]);
-        const v1: i32 = @intCast(l1[i]);
+    // std.debug.print("len is {d}\n", .{l1.items.len});
+
+    var t: u32 = 0;
+    for (0..l1.items.len) |i| {
+        // std.debug.print("{d} ", .{i});
+        const v2: i32 = @intCast(l2.items[i]);
+        const v1: i32 = @intCast(l1.items[i]);
         const r = @abs(v2 - v1);
         t += r;
     }
 
-    return t;
+    return @intCast(t);
 }
 fn part2(data: []u8) anyerror!i32 {
     _ = data;
