621cfc5a6374f9f5238a2b97590f60e9d133ec35
Samuel Ibarra <ing.samuelibarra@gmail.com>
2025-06-27 17:32:47 -0500

    add day 04 rust

--- rust/2024/src/days/day04.rs
+++ rust/2024/src/days/day04.rs
@@ -1,32 +1,32 @@
 use crate::advent_utils::Solution as AdventSolution;
 
 // #[derive(PartialEq)]
-enum Dir {
-    Up,
-    Down,
-    Left,
-    Right,
-    UpLeft,
-    UpRight,
-    DownLeft,
-    DownRight,
-}
+// enum Dir {
+//     Up,
+//     Down,
+//     Left,
+//     Right,
+//     UpLeft,
+//     UpRight,
+//     DownLeft,
+//     DownRight,
+// }
 
-impl Dir {
-    fn delta(&self) -> (i8, i8) {
-        let yi = match self {
-            Dir::Up | Dir::UpLeft | Dir::UpRight => -1,
-            Dir::Down | Dir::DownLeft | Dir::DownRight => 1,
-            _ => 0,
-        };
-        let xi = match self {
-            Dir::Left | Dir::UpLeft | Dir::DownLeft => -1,
-            Dir::Right | Dir::UpRight | Dir::DownRight => 1,
-            _ => 0,
-        };
-        (xi, yi)
-    }
-}
+// impl Dir {
+//     fn delta(&self) -> (i8, i8) {
+//         let yi = match self {
+//             Dir::Up | Dir::UpLeft | Dir::UpRight => -1,
+//             Dir::Down | Dir::DownLeft | Dir::DownRight => 1,
+//             _ => 0,
+//         };
+//         let xi = match self {
+//             Dir::Left | Dir::UpLeft | Dir::DownLeft => -1,
+//             Dir::Right | Dir::UpRight | Dir::DownRight => 1,
+//             _ => 0,
+//         };
+//         (xi, yi)
+//     }
+// }
 pub struct Solution;
 
 impl AdventSolution for Solution {
@@ -38,8 +38,60 @@ impl AdventSolution for Solution {
     /// let input = std::fs::read_to_string("../../tests/2024/day044.txt").unwrap();
     /// assert_eq!(days::day04::Solution::part_1(&input), 4);
     /// ```
-    fn part_1(_data: &str) -> usize {
-        todo!()
+    fn part_1(data: &str) -> usize {
+        let mut tot = 0;
+        let bytes = data.as_bytes();
+        let xmax = data.find("\n").expect("no lines found?");
+        let ymax = data.len() / (xmax + 1);
+        println!("xmax is {xmax} and ymax is {ymax}");
+
+        let idx = |x, y| x + y * (xmax + 1);
+
+        for (y, line) in data.lines().enumerate() {
+            let mut last = 0;
+            while let Some(x) = line[last..].find('X').map(|x| x + last) {
+                last = x + 1;
+
+                tot += (x < xmax - 3
+                    && bytes[idx(x + 1, y)] == b'M'
+                    && bytes[idx(x + 2, y)] == b'A'
+                    && bytes[idx(x + 3, y)] == b'S') as usize
+                    + (x > 2
+                        && bytes[idx(x - 1, y)] == b'M'
+                        && bytes[idx(x - 2, y)] == b'A'
+                        && bytes[idx(x - 3, y)] == b'S') as usize
+                    + (x < xmax - 3
+                        && y < ymax - 3
+                        && bytes[idx(x + 1, y + 1)] == b'M'
+                        && bytes[idx(x + 2, y + 2)] == b'A'
+                        && bytes[idx(x + 3, y + 3)] == b'S') as usize
+                    + (x > 2
+                        && y > 2
+                        && bytes[idx(x - 1, y - 1)] == b'M'
+                        && bytes[idx(x - 2, y - 2)] == b'A'
+                        && bytes[idx(x - 3, y - 3)] == b'S') as usize
+                    + (x < xmax - 3
+                        && y > 2
+                        && bytes[idx(x + 1, y - 1)] == b'M'
+                        && bytes[idx(x + 2, y - 2)] == b'A'
+                        && bytes[idx(x + 3, y - 3)] == b'S') as usize
+                    + (x > 2
+                        && y < ymax - 3
+                        && bytes[idx(x - 1, y + 1)] == b'M'
+                        && bytes[idx(x - 2, y + 2)] == b'A'
+                        && bytes[idx(x - 3, y + 3)] == b'S') as usize
+                    + (y < ymax - 3
+                        && bytes[idx(x, y + 1)] == b'M'
+                        && bytes[idx(x, y + 2)] == b'A'
+                        && bytes[idx(x, y + 3)] == b'S') as usize
+                    + (y > 2
+                        && bytes[idx(x, y - 1)] == b'M'
+                        && bytes[idx(x, y - 2)] == b'A'
+                        && bytes[idx(x, y - 3)] == b'S') as usize;
+            }
+        }
+
+        tot
     }
     /// ```
     /// # use aoc2024::days;
